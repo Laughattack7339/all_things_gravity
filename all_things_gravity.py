41a0584c9e082a6d1003f153b2111927ed118157
Laughattack7339 <168693203+Laughattack7339@users.noreply.github.com>
2024-05-11 11:22:10 -0500

    Update all_things_gravity.py

--- all_things_gravity.py
+++ all_things_gravity.py
@@ -1,4 +1,5 @@
 
+
 import pygame
 pygame.init()
 import time
@@ -11,26 +12,33 @@ height = 600
 running = True
 screen = pygame.display.set_mode ((width, height))
 background = pygame.image.load('testing_back.png').convert_alpha()
-
+player_y = 609
+player_x = 442
 jumping = False
 jump_key_pressed = False
 player_image = pygame.image.load("robo_cycle.png")
-player_rect = player_image.get_rect(midbottom=(442, 609))
+player_rect = player_image.get_rect(midbottom=(player_x, player_y))
 question_block = pygame.image.load("question_block.png")
-queston_rect = question_block.get_rect(midbottom=(442, 609))
-player_x = 442
-player_y = 500
+question_rect = question_block.get_rect(midbottom=(150, 550))
+
 gravity = 1
 jump_height = 20
 velocity = jump_height
 start_time = pygame.time.get_ticks()
 clock = pygame.time.Clock()
-
+player_color = (255, 0, 0)
+question_color = (23, 234, 165)
+player_rect_outline = pygame.Rect(player_rect.left, player_rect.right, player_rect.width, player_rect.height)
+player_rect_outline.inflate_ip(-4, -4)
+queston_rect_outline = pygame.Rect(question_rect.left, question_rect.right, question_rect.width, question_rect.height)
+queston_rect_outline.inflate_ip(-4, -4)
+player_hitbox = player_rect.inflate(-550, -550)
+question_hitbox = question_rect.inflate(-550, -550)
 
 while running:
     screen.blit(background, (0, 0))
 
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -46,8 +54,9 @@ while running:
         if velocity < -jump_height:
             velocity = jump_height
             jumping = False
-
-   
+    collision = pygame.Rect.colliderect(player_rect, question_rect)
+    if collision:
+        print(collision)
 
     
 
@@ -56,8 +65,11 @@ while running:
             
         
 
-    screen.blit(question_block, queston_rect)
+    screen.blit(question_block, question_rect)
     screen.blit(player_image, player_rect)
+
+    pygame.draw.rect(screen, player_color, player_hitbox, 4)
+    pygame.draw.rect(screen, question_color, question_hitbox, 4)
 
     pygame.display.flip()
     clock.tick(60)
