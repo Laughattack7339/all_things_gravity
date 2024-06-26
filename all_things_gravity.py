794f37aa0c588f0958a1f42354741f1be8d686c6
Laughattack7339 <168693203+Laughattack7339@users.noreply.github.com>
2024-06-25 21:32:24 -0500

    Update all_things_gravity.py

--- all_things_gravity.py
+++ all_things_gravity.py
@@ -22,7 +22,10 @@ player_image = pygame.image.load("robo_cycle.png")
 player_rect = player_image.get_rect(midbottom=(player_x, player_y))
 question_block = pygame.image.load("question_block.png")
 question_rect = question_block.get_rect(midbottom=(100, 300))
-
+platform_big1 = pygame.image.load("platformbig.png")
+big_plat_rect1 = platform_big1.get_rect(midbottom=(276, 385))
+floor_image = pygame.image.load('floor.png')
+floor_rect = floor_image.get_rect(midbottom=(500, 600))
 gravity = 1
 jump_height = 20
 velocity = jump_height
@@ -34,49 +37,86 @@ player_rect_outline = pygame.Rect(player_rect.left, player_rect.right, player_re
 player_rect_outline.inflate_ip(-4, -4)
 queston_rect_outline = pygame.Rect(question_rect.left, question_rect.right, question_rect.width, question_rect.height)
 queston_rect_outline.inflate_ip(-4, -4)
-player_hitbox = player_rect.inflate(-550, -550)
-question_hitbox = question_rect.inflate(-550, -550)
+iscolliding_question = False
+iscolliding_bigplat1 = False
 coins = 0
+isfalling = False
 
 
 
 def handle_events():
-    global jumping, velocity
+    global jumping, velocity, iscolliding_bigplat1, iscolliding_question
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and not jumping:
                 jumping = True
+                iscolliding_bigplat1 = False
+                iscolliding_question = False
                 velocity = -jump_height
 
     return True
     
 def update_positions():
-    global velocity, jumping, coins, keys
+    global velocity, jumping, coins, keys, iscolliding_bigplat1, iscolliding_question, isfalling, floor_rect
+    
+    if player_rect.colliderect(floor_rect) and isfalling:
+        player_rect.bottom = floor_rect.top
+        velocity = gravity
+        jumping = False
+
     if jumping:
         player_rect.y += velocity
         velocity += gravity
-        if player_rect.bottom >= height:
-            player_rect.bottom = height
 
-        if player_rect.colliderect(question_rect):
-            if velocity > 0:
-                jumping = False
-                player_rect.y = question_rect.bottom
+    if velocity > 0:
+        isfalling = True
+
+    if velocity < 0:
+        isfalling = False
+        
+#question rect stuff begins
+
+        if player_rect.colliderect(question_rect) and velocity > 0:
+            iscolliding_question = True
+        
+        if iscolliding_question:
+            if isfalling:
+                player_rect.bottom = question_rect.top
                 velocity = 0
-
-       
-        
+                jumping = False
 
         
-            
+        
+        if iscolliding_question:
+             if not isfalling:
+                player_rect.top = question_rect.bottom
+                velocity = gravity
+                
 
+#big plat stuff begins        
+        if player_rect.colliderect(big_plat_rect1):
+             iscolliding_bigplat1 = True
+        
+        if iscolliding_bigplat1:     
+            if not isfalling:
+                player_rect.top = big_plat_rect1.bottom
+                velocity = gravity
 
         
+             
+        if iscolliding_bigplat1:
+             if isfalling:
+                  player_rect.bottom = big_plat_rect1.top
+                  velocity = 0
+                  jumping = False
 
-
-
+        
+        
+        
+        
+                        
 
 running = True
 
@@ -86,6 +126,9 @@ while running:
     screen.blit(background, (0, 0))
     screen.blit(question_block, question_rect)
     screen.blit(player_image, player_rect)
+    screen.blit(floor_image, floor_rect)
+
+    screen.blit(platform_big1, big_plat_rect1)
 
     update_positions()
 
@@ -94,9 +137,25 @@ while running:
 
     elif keys [pygame.K_a] == True:
             player_x -= 5
-            print("pressed")
+
+    elif keys [pygame.K_1] == True:
+         print("velovity ", velocity)
+         print("gravity ", gravity)
+         print("player x ", player_x)
+         print("player y ", player_rect.bottom)
+         print("falling ", isfalling)
+         print("floor ", floor_rect.top)
 
     player_rect.x = player_x
+   
+    
+    if player_x > 1000:
+        player_x = 0
+
+    if player_x < 0:
+        player_x = 1000
+
+    
             
         
 
