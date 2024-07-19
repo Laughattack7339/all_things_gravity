3b79c7e3173413c696e1183be4f49e0476c750d1
Laughattack7339 <168693203+Laughattack7339@users.noreply.github.com>
2024-07-19 13:58:48 -0500

    Update all_things_gravity.py

--- all_things_gravity.py
+++ all_things_gravity.py
@@ -1,167 +1,202 @@
-
-
 import pygame
 pygame.init()
-import time
 
 
 width = 1000
 height = 600
 
 
-running = True
-screen = pygame.display.set_mode ((width, height))
+screen = pygame.display.set_mode((width, height))
+
 background = pygame.image.load('testing_back.png').convert_alpha()
-y = 492
-x = 100
-player_y = y
-player_x = x
-jumping = False
-jump_key_pressed = False
 player_image = pygame.image.load("robo_cycle.png")
-player_rect = player_image.get_rect(midbottom=(player_x, player_y))
-question_block = pygame.image.load("question_block.png")
-question_rect = question_block.get_rect(midbottom=(100, 300))
 platform_big1 = pygame.image.load("platformbig.png")
-big_plat_rect1 = platform_big1.get_rect(midbottom=(276, 385))
 floor_image = pygame.image.load('floor.png')
+small_mover1 = pygame.image.load('little_mover.png')
+small_plat = pygame.image.load('smol_platl.png')
+space = pygame.image.load('space.png')
+
+frame_width = 36
+frame_height = 64
+num_frames = 12
+
+
+
+
+player_rect = player_image.get_rect(midbottom=(100, 492))
+big_plat_rect1 = platform_big1.get_rect(midbottom=(276, 385))
 floor_rect = floor_image.get_rect(midbottom=(500, 600))
+small_mover1_rect = small_mover1.get_rect(midbottom=(700, 250))
+small_plat_rect = small_plat.get_rect(midbottom=(300, 200))
+
+
 gravity = 1
 jump_height = 20
+
 velocity = jump_height
-start_time = pygame.time.get_ticks()
 clock = pygame.time.Clock()
-player_color = (255, 0, 0)
-question_color = (23, 234, 165)
-player_rect_outline = pygame.Rect(player_rect.left, player_rect.right, player_rect.width, player_rect.height)
-player_rect_outline.inflate_ip(-4, -4)
-queston_rect_outline = pygame.Rect(question_rect.left, question_rect.right, question_rect.width, question_rect.height)
-queston_rect_outline.inflate_ip(-4, -4)
-iscolliding_question = False
-iscolliding_bigplat1 = False
+
+jumping = False
+isfalling = True
 coins = 0
-isfalling = False
+show_hitboxes = False
+moving_right = True
+in_space = False
 
-
+animation_index = 0
+animation_speed = 0.1
+animation_timer = 0
 
 def handle_events():
-    global jumping, velocity, iscolliding_bigplat1, iscolliding_question
+    global jumping, velocity, show_hitboxes
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and not jumping:
                 jumping = True
-                iscolliding_bigplat1 = False
-                iscolliding_question = False
                 velocity = -jump_height
-
+            elif event.key == pygame.K_h:
+                show_hitboxes = not show_hitboxes
     return True
-    
-def update_positions():
-    global velocity, jumping, coins, keys, iscolliding_bigplat1, iscolliding_question, isfalling, floor_rect
-    
-    if player_rect.colliderect(floor_rect) and isfalling:
-        player_rect.bottom = floor_rect.top
-        velocity = gravity
-        jumping = False
 
+def update_positions():
+    global velocity, jumping, isfalling, gravity, in_space
+    in_space = False
     if jumping:
         player_rect.y += velocity
         velocity += gravity
 
-    if velocity > 0:
-        isfalling = True
-
-    if velocity < 0:
+   
+    if player_rect.colliderect(floor_rect):
+        player_rect.bottom = floor_rect.top
+        velocity = gravity
+        jumping = False
         isfalling = False
-        
-#question rect stuff begins
 
-        if player_rect.colliderect(question_rect) and velocity > 0:
-            iscolliding_question = True
+    
+   
+   
+    if player_rect.colliderect(big_plat_rect1):
+        if velocity > 0:  
+            player_rect.bottom = big_plat_rect1.top
+            velocity = 0
+            jumping = False
         
-        if iscolliding_question:
-            if isfalling:
-                player_rect.bottom = question_rect.top
-                velocity = 0
-                jumping = False
+         
+        else: 
+            player_rect.top = big_plat_rect1.bottom
+            velocity = gravity
 
-        
-        
-        if iscolliding_question:
-             if not isfalling:
-                player_rect.top = question_rect.bottom
-                velocity = gravity
-                
+    
+    if player_rect.colliderect(big_plat_rect1):
+        if player_rect.left == big_plat_rect1.right:
+            player_rect.left = big_plat_rect1.right
+    
+    
+    if player_rect.colliderect(small_mover1_rect):
+        if velocity > 0:
+            player_rect.bottom = small_mover1_rect.top
+            velocity = 0
+            jumping = False
 
-#big plat stuff begins        
-        if player_rect.colliderect(big_plat_rect1):
-             iscolliding_bigplat1 = True
-        
-        if iscolliding_bigplat1:     
-            if not isfalling:
-                player_rect.top = big_plat_rect1.bottom
-                velocity = gravity
+        else: 
+            player_rect.top = small_mover1_rect.bottom
+            velocity = gravity
 
-        
-             
-        if iscolliding_bigplat1:
-             if isfalling:
-                  player_rect.bottom = big_plat_rect1.top
-                  velocity = 0
-                  jumping = False
+    if player_rect.colliderect(small_plat_rect):
+        if velocity > 0:
+            player_rect.bottom = small_plat_rect.top
+            velocity = 0
+            jumping = False
 
-        
-        
-        
-        
-                        
+        else:
+            player_rect.top = small_plat_rect.bottom        
+            velocity = gravity
+
+
+    if not player_rect.colliderect(floor_rect) and not player_rect.colliderect(big_plat_rect1):
+        isfalling = True
+    else:
+        isfalling = False
+
+    if isfalling and not jumping:
+        velocity += gravity
+        player_rect.y += velocity
+
+    
+
+
+    
+
+def move_platform():
+    global moving_right
+    if moving_right:
+        small_mover1_rect.x += 1
+        if small_mover1_rect.x >= 750:
+            moving_right = False
+    else:
+        small_mover1_rect.x -= 1
+        if small_mover1_rect.x <= 550:
+            moving_right = True
+
+    
+
+
+
+
+
+
 
 running = True
 
 while running:
-    keys = pygame.key.get_pressed()
     running = handle_events()
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_d]:
+        player_rect.x += 5
+    if keys[pygame.K_a]:
+        player_rect.x -= 5
+    if keys[pygame.K_1]:
+        print(f"velocity: {velocity}, gravity: {gravity}, player x: {player_rect.x}, player y: {player_rect.y}, falling: {isfalling}, floor: {floor_rect.top}")
+        print(small_mover1_rect.x)
+    update_positions()
+    move_platform()
+    
+
+    
+    
+    if player_rect.right > width:
+        player_rect.left = 0
+        
+    if player_rect.left < 0:
+        player_rect.right = 1001
+
+    
     screen.blit(background, (0, 0))
-    screen.blit(question_block, question_rect)
     screen.blit(player_image, player_rect)
     screen.blit(floor_image, floor_rect)
-
     screen.blit(platform_big1, big_plat_rect1)
-
-    update_positions()
-
-    if keys [pygame.K_d] == True:
-            player_x += 5
-
-    elif keys [pygame.K_a] == True:
-            player_x -= 5
-
-    elif keys [pygame.K_1] == True:
-         print("velovity ", velocity)
-         print("gravity ", gravity)
-         print("player x ", player_x)
-         print("player y ", player_rect.bottom)
-         print("falling ", isfalling)
-         print("floor ", floor_rect.top)
-
-    player_rect.x = player_x
-   
+    screen.blit(small_mover1, small_mover1_rect)
+    screen.blit(small_plat, small_plat_rect)
     
-    if player_x > 1000:
-        player_x = 0
-
-    if player_x < 0:
-        player_x = 1000
-
+    if show_hitboxes:
+        pygame.draw.rect(screen, (255, 0, 0), player_rect, 2)         
+        pygame.draw.rect(screen, (0, 0, 255), big_plat_rect1, 2)    
+        pygame.draw.rect(screen, (255, 255, 0), floor_rect, 2)
     
-            
-        
+    if player_rect.y <= 0:
+        in_space = True
+
+    if in_space:
+        player_rect.y = 599
+        screen.blit(space, (0, 0))
+        screen.blit(player_image, player_rect)
+    
 
     
     pygame.display.flip()
     clock.tick(60)
+
 pygame.quit()
-
-
